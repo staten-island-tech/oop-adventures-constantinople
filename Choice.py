352b0112ce3286747dcf72bfa50baebc60b69ed2
Matthewv211 <124072050+Matthewv211@users.noreply.github.com>
2023-04-25 10:57:12 -0400

    beginning

---
+++ Choice.py
@@ -0,0 +1,4 @@
+def beginning():
+    print("You have now been transported into the world of rogue lineage. ")
+    input("Would you like to become an orderly, neutral, or chaotic class? (Type as 'Orderly' 'Neutral' or 'Chaotic') ")
+beginning()
