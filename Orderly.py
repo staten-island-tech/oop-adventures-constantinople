8de057e2ab504bbcca8318c6ce709873d321f4c4
Constantine-nagornuk <conplay55@gmail.com>
2023-04-26 10:46:12 -0400

    Update Orderly.py

--- Orderly.py
+++ Orderly.py
@@ -7,13 +7,18 @@ class Base_class:
     def info(self):
         print(self.HP , self.Weapon , self.Money , self.XP)
 
-
+enemy = int(150)
 
 class sigil_Knight(Base_class):
     def __init__(self, HP, Weapon, Money, XP):
         super().__init__(HP, Weapon, Money, XP)
-    
+    def Attack(self):
+        Damage = int(75)
+        Remaing_enemy_HP = enemy - Damage
+        print(Remaing_enemy_HP)
 A = sigil_Knight(100, "longsword" , 0 , 0)
 A.info()
+A.Attack()
+
 
 
