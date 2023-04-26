f2f8c1a1d27f67439c2dc8f8ba73385d7ba2b280
Constantine-nagornuk <conplay55@gmail.com>
2023-04-26 10:29:03 -0400

    Create Orderly.py

---
+++ Orderly.py
@@ -0,0 +1,10 @@
+class Base_class:
+    def __init__(self, HP , Weapon , Money , XP):
+        self.HP = HP
+        self.Weapon = Weapon
+        self.Money = Money
+        self.XP = XP
+
+
+class sigil_Knight(Base_class):
+    
