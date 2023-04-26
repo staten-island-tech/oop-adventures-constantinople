bb87b916672dfa9719ac2be15f3c008fa4894845
Constantine-nagornuk <conplay55@gmail.com>
2023-04-26 10:22:40 -0400

    base

--- Classes.py
+++ Classes.py
@@ -1,2 +1,8 @@
-class Sigil_Knight:
-    
+class Base_class:
+    def __init__(self, HP , Weapon , Ability, Money , XP):
+        self.HP = HP
+        self.Weapon = Weapon
+        self.Ability = Ability
+        self.Money = Money
+        self.XP = XP
+
