9ef915b97148e4716e51b8c7c2b3182b524dae59
Constantine-nagornuk <conplay55@gmail.com>
2023-04-26 10:41:05 -0400

    Update Orderly.py

--- Orderly.py
+++ Orderly.py
@@ -1,10 +1,19 @@
 class Base_class:
     def __init__(self, HP , Weapon , Money , XP):
-        self.HP = HP
+        self.HP = int(HP)
         self.Weapon = Weapon
-        self.Money = Money
-        self.XP = XP
+        self.Money = int(Money)
+        self.XP = int(XP)
+    def info(self):
+        print(self.HP , self.Weapon , self.Money , self.XP)
+
 
 
 class sigil_Knight(Base_class):
+    def __init__(self, HP, Weapon, Money, XP):
+        super().__init__(HP, Weapon, Money, XP)
     
+A = sigil_Knight(100, "longsword" , 0 , 0)
+A.info()
+
+
