86e5b78d3f90192fe8c0203e162da9c248932196
Constantine-nagornuk <conplay55@gmail.com>
2023-04-27 11:01:17 -0400

    Update Orderly.py

--- Orderly.py
+++ Orderly.py
@@ -8,31 +8,37 @@ class Base_class:
     def info(self):
         print(self.HP , self.Weapon , self.Money , self.XP)
 
-enemy = int(150)
+class Enemy1:
+    def __init__(self, HP , XP):
+        self.HP = HP
+        self.XP = XP
+B = Enemy1(150 , 25 )
+
 
 class sigil_Knight(Base_class):
     def __init__(self, HP, Weapon, Money, XP):
         super().__init__(HP, Weapon, Money, XP)
     def Attack(self):
-        Number_of_hits = random.randint(0 , 5)
-        Damage = int(25)
-        Total_DMG = Number_of_hits*Damage
-        Remaing_enemy_HP = enemy - Total_DMG
-        print(Remaing_enemy_HP)
+        while B.HP > 0:
+            if B.HP < 0:
+                break  
+            OL = input('Would you like to attack?').upper()
+            if OL == 'YES':
+                Number_of_hits = random.randint(0 , 5)
+                Damage = int(25)
+                Total_DMG = Number_of_hits*Damage
+                Remaing_enemy_HP = B.HP - Total_DMG
+                if Remaing_enemy_HP < 0:
+                    print('You defeated this enemy')
+                    Added_XP = B.XP + A.XP
+                    print('You now have,' , Added_XP , 'XP')
+                    break
+                B.HP = Remaing_enemy_HP
+                print(Remaing_enemy_HP)
+                
+                
 
-A = sigil_Knight(100, "longsword" , 0 , 0)
+A = sigil_Knight(100, x, 0 , 0)
 
-OL = input('Would you like to attack?')
-OL.upper()
-print(OL)
-if OL == 'YES':
-    A.Attack()
-elif OL:
-    print('You backed away from the attack')
-
-
-
-
-
-
+A.Attack()
 
