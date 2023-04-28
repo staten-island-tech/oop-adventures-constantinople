1ea5256c70eddc1cb5c9be3bbb3f83955945895b
Constantine-nagornuk <124071530+Constantine-nagornuk@users.noreply.github.com>
2023-04-27 21:04:44 -0400

    Update Orderly.py

--- Orderly.py
+++ Orderly.py
@@ -7,7 +7,24 @@ class Base_class:
         self.XP = int(XP)
     def info(self):
         print(self.HP , self.Weapon , self.Money , self.XP)
-
+    def Attack(self):
+        while B.HP > 0:
+            if B.HP < 0:
+                break  
+            OL = input('Would you like to attack?').upper()
+            if OL == 'YES':
+                Number_of_hits = random.randint(0 , 5)
+                Damage = int(25)
+                Total_DMG = Number_of_hits*Damage
+                Remaing_enemy_HP = B.HP - Total_DMG
+                if Remaing_enemy_HP < 0:
+                    print('You defeated this enemy')
+                    Added_XP = B.XP + A.XP
+                    A.XP = Added_XP
+                    print('You now have,' , Added_XP , 'XP')
+                    break
+                B.HP = Remaing_enemy_HP
+                print(Remaing_enemy_HP)
 
 
 
@@ -17,8 +34,6 @@ class Enemy1:
         self.HP = HP
         self.XP = XP
 B = Enemy1(150 , 25 )
-
-
 
 class sigil_Knight(Base_class):
     def __init__(self, HP, Weapon, Money, XP):
@@ -42,9 +57,31 @@ class sigil_Knight(Base_class):
                 B.HP = Remaing_enemy_HP
                 print(Remaing_enemy_HP)
                 
-                
+class Whisperer(Base_class):
+    def __init__(self, HP, Weapon, Money, XP):
+        super().__init__(HP, Weapon, Money, XP)             
+def Attack(self):
+        while C.HP > 0:
+            if C.HP < 0:
+                break  
+            OL = input('Would you like to attack?').upper()
+            if OL == 'YES':
+                Number_of_hits = random.randint(0 , 3)
+                Damage = int(15)
+                Total_DMG = Number_of_hits*Damage
+                Remaing_enemy_HP = C.HP - Total_DMG
+                if Remaing_enemy_HP < 0:
+                    print('You defeated this enemy')
+                    Added_XP = B.XP + C.XP
+                    C.XP = Added_XP
+                    print('You now have,' , Added_XP , 'XP')
+                    break
+                B.HP = Remaing_enemy_HP
+                print(Remaing_enemy_HP)
+
 
 A = sigil_Knight(100, 'x', 0 , 0)
-
-A.Attack()
+C = Whisperer(125, 'somthing' , 0 , 0)
+A.Attack
+C.Attack()
 print(A.XP)
