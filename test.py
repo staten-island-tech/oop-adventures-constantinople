db4fcf7c0bd632537832f4eeed260b9be3a7c6ff
Constantine-nagornuk <conplay55@gmail.com>
2023-05-09 10:22:42 -0400

    Update test.py

--- test.py
+++ test.py
@@ -11,9 +11,6 @@ Arcane.Special_Move(po)
 
 """ if you want to acces anything import it in and acces it all or whatever you need such as example above """
 
-Arcane = boss(450 , ' Mace', 100 )
-Arcane.Special_Move(po)
-
 
 
 
