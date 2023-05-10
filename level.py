cf82149b4ba2431e5dfa2d7edd04e3546f5718e1
Constantine-nagornuk <conplay55@gmail.com>
2023-05-10 11:01:33 -0400

    level 1.1 update
    broken

--- level.py
+++ level.py
@@ -6,15 +6,17 @@ class level:
         pass
     def level_print(self):
         global current_level 
-        if level == range(0,51):
+        global level
+        for level in range(0,51):
+            global current_level
             current_level ='Tier 1'       
-        elif level == range(51,101):
+        for level in range(51,101):
             current_level = 'Tier_2'
-        elif level == range(101,251):
+        for level in range(101,251):
             current_level = 'Tier_3'
-        elif level == range(251,351):
+        for level in range(251,351):
             current_level = 'Tier_4'
-        elif level == range(351,1000000):
+        for level in range(351,1000000):
             current_level = 'Tier_5'
         print(current_level)
  
