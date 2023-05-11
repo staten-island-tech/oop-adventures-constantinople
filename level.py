4729a09106e0ecf951a222d0eeded9c765b66c21
Constantine-nagornuk <conplay55@gmail.com>
2023-05-11 10:40:50 -0400

    level 1.3
    sort of working

--- level.py
+++ level.py
@@ -1,24 +1,32 @@
-level = int(40)
-current_level = ''
+
+x = 324
+
 
 class level:
     def __init__(self) -> None:
         pass
     def level_print(self):
-        global current_level 
-        global level
-        for level in range(0,51):
-            global current_level
-            current_level ='Tier 1'       
-        for level in range(51,101):
-            current_level = 'Tier_2'
-        for level in range(101,251):
-            current_level = 'Tier_3'
-        for level in range(251,351):
-            current_level = 'Tier_4'
-        for level in range(351,1000000):
-            current_level = 'Tier_5'
-        print(current_level)
- 
+        if x  >= 0 and x <= 100:
+            tag = 'Tier_1'
+            print(tag)
+        elif x  >= 101 and x <= 200:
+            tag = 'Tier_2'
+            print(tag)
+        elif x >= 201 and x <= 300:
+            tag = 'Tier_3'
+            print(tag)
+        elif x >= 301 and x <= 500:
+            tag = 'Tier_4'
+            print(tag)
+        elif x >= 501 and x <= 100000:
+            tag = 'Tier_5'
+            print(tag)
+
+
+
+
 TEST = level()
 TEST.level_print()
+
+
+
