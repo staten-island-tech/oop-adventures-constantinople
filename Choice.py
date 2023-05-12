25f26a63f17fa62fea9d6786e5b56986773255f8
Matthewv211 <124072050+Matthewv211@users.noreply.github.com>
2023-05-12 10:58:49 -0400

    FINALLY

--- Choice.py
+++ Choice.py
@@ -1,4 +1,17 @@
-def beginning():
-    print("You have now been transported into the world of rogue lineage. ")
-    input("Would you like to become an orderly, neutral, or chaotic class? (Type as 'Orderly' 'Neutral' or 'Chaotic') ")
-beginning()
+class start:
+    def beginning():
+        print("You have now been transported into the world of an ancient lineage. ")
+        everything = input("Would you like to become an orderly, neutral, or chaotic class?").upper()
+        if everything == "ORDERLY":
+            import 
+        elif everything == "CHAOTIC":
+            import 
+        elif everything == "NEUTRAL":
+            import 
+        else:
+            print("Iagnime not bineg albe to selpl...")
+            start.beginning()
+start.beginning()
+
+
+
