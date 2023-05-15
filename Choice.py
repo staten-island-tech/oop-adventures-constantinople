764b5d4a100e6ef35946d2389a4dea0327514c27
Matthewv211 <124072050+Matthewv211@users.noreply.github.com>
2023-05-15 10:48:54 -0400

    oh no oh no

--- Choice.py
+++ Choice.py
@@ -1,17 +1,40 @@
+import Orderly
+from Orderly import *
+import Chaotic
+from Chaotic import *
+import Neutral
+from Neutral import *
 class start:
     def beginning():
         print("You have now been transported into the world of an ancient lineage. ")
         everything = input("Would you like to become an orderly, neutral, or chaotic class? ").upper()
         if everything == "ORDERLY":
-            import Orderly
-            from Orderly import *
             class1 = input("What class would you like to progress in?: 'Sigil Knight' or 'Whisperer' ").upper()
             if class1 == "SIGIL KNIGHT": 
-                user_class = A
+                print(A)
+            elif class1 == "WHISPERER":
+                user_class = C
+            else:
+                print("Iagnime not bineg albe to selpl...")
+                start.beginning()
         elif everything == "CHAOTIC":
-            import Chaotic
+            class2 = input("What class would you like to progress in?: 'Oni' or 'Faceless' ").upper()
+            if class2 == "ONI":
+                user_class = op
+            elif class2 == "FACELESS":
+                user_class = po
+            else:
+                print("Iagnime not bineg albe to selpl...")
+                start.beginning()
         elif everything == "NEUTRAL":
-            import Neutral
+            class3 = input("What class would you like to progress in?: 'Chef' or 'Blacksmith' ").upper()
+            if class3 == "CHEF":
+                user_class = g
+            elif class3 == "BLACKSMITH":
+                user_class = y
+            else:
+                print("Iagnime not bineg albe to selpl...")
+                start.beginning()
         else:
             print("Iagnime not bineg albe to selpl...")
             start.beginning()
