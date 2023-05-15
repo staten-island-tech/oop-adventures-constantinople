6e0019c11740e7efa2e475045bb363e506596ab1
Matthewv211 <124072050+Matthewv211@users.noreply.github.com>
2023-05-15 10:37:07 -0400

    oh no

--- Choice.py
+++ Choice.py
@@ -1,9 +1,13 @@
 class start:
     def beginning():
         print("You have now been transported into the world of an ancient lineage. ")
-        everything = input("Would you like to become an orderly, neutral, or chaotic class?").upper()
+        everything = input("Would you like to become an orderly, neutral, or chaotic class? ").upper()
         if everything == "ORDERLY":
             import Orderly
+            from Orderly import *
+            class1 = input("What class would you like to progress in?: 'Sigil Knight' or 'Whisperer' ").upper()
+            if class1 == "SIGIL KNIGHT": 
+                user_class = A
         elif everything == "CHAOTIC":
             import Chaotic
         elif everything == "NEUTRAL":
@@ -12,6 +16,5 @@ class start:
             print("Iagnime not bineg albe to selpl...")
             start.beginning()
 start.beginning()
-
 
 
