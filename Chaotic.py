13ebec49298f09c1c1b0f9b917c34240bc4164fe
Constantine-nagornuk <conplay55@gmail.com>
2023-05-16 11:00:40 -0400

    Combat overhaul 1.0

--- Chaotic.py
+++ Chaotic.py
@@ -2,33 +2,53 @@ import random
 import base_file
 from base_file import Base_class, B
 
+bronze_sword = int(9)
+silver_sword = int(10)
+mythril_sword = int(14)
+fists = int(6)
+dagger = int(9)
+rapier = int(11)
+fists2 = int(11)
+bronze_Dagger = int(7) 
+silver_Dagger = int (10)
+mythril_Dagger = int (13)
+bronze_Pan = int(5)
+silver_Pan = int(9)
+mythril_Pan = int(12)
+bronze_Hammer = int(8) 
+silver_Hammer = int(10)
+mythril_Hammer = int(13)
+
+
 
 class Faceless(Base_class):
     def __init__(self, HP, Weapon, XP):
         super().__init__(HP, Weapon, XP)
-    def Attack(self):
-        while B.HP > 0:
-            if B.HP < 0:
+    def Attack(self,x,y):
+        while x.HP > 0:
+            if x.HP < 0:
                 break  
-            OL = input('Damage the enemy?').upper()
+            OL = input('Swing your weapon at the enemy?').upper()
             if OL == 'YES':
                 Number_of_hits = random.randint(0 , 4)
-                Damage = int(30)
+                Damage = y
                 Total_DMG = Number_of_hits*Damage
-                Remaing_enemy_HP = B.HP - Total_DMG
+                Remaing_enemy_HP = x.HP - Total_DMG
                 if Remaing_enemy_HP < 0 or Remaing_enemy_HP == 0:
                     print('You defeated this enemy')
-                    Added_XP = B.XP + po.XP
+                    Added_XP = x.XP + po.XP
                     po.XP = Added_XP
                     print('You now have,' , Added_XP , 'XP')
                     break
-                B.HP = Remaing_enemy_HP
+                x.HP = Remaing_enemy_HP
                 print(Remaing_enemy_HP)
             if OL != 'YES':
                 print('You dont attack')
                 break    
 
 po = Faceless(105 , 'something' , 0)
+
+po.Attack(B,silver_Dagger)
 
 class oni(Base_class):
     def __init__(self, HP, Weapon, XP):
