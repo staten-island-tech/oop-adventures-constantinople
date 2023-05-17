f01fbe2be2439f7226d59a2b030e871db8c397a3
Constantine-nagornuk <conplay55@gmail.com>
2023-05-17 10:30:00 -0400

    ww

---
+++ weapons.py
@@ -0,0 +1,22 @@
+pol = input('test').upper()
+if  pol == 'SILVER_HAMMER':
+    po.Weapon = silver_Hammer
+po.Attack(B,po.Weapon)
+
+
+bronze_sword = int(9)
+silver_sword = int(10)
+mythril_sword = int(14)
+fists = int(6)
+dagger = int(9)
+rapier = int(11)
+fists2 = int(11)
+bronze_Dagger = int(7) 
+silver_Dagger = int (10)
+mythril_Dagger = int (13)
+bronze_Pan = int(5)
+silver_Pan = int(9)
+mythril_Pan = int(12)
+bronze_Hammer = int(8) 
+silver_Hammer = int(10)
+mythril_Hammer = int(13)
