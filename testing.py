f0aa931c55e98be7f7c0973714fbf076cfec592e
Constantine-nagornuk <conplay55@gmail.com>
2023-05-25 10:58:51 -0400

    replay system starting

--- testing.py
+++ testing.py
@@ -1,36 +1,24 @@
-
-
-import random
-import base_file 
-from base_file import *
 import Chaotic
-from Chaotic import *
-import boss 
-from boss import *
-import Neutral
-from Neutral import *
-import Orderly
-from Orderly import *
+from Chaotic import po
+import base_file
+from base_file import B
+import weapons
+from weapons import bronze_Dagger
 
+test = po.Attack(B, bronze_Dagger)
 
-""" Arcane = boss(450 , ' Mace', 100 )
-Arcane.Special_Move(po)
- """
-""" if you want to acces anything import it in and acces it all or whatever you need such as example above """
-
-
-po.info()
-print('Your Current level is:')
-po.level_print()
-replay1.replay(po)
-print('Your Current level is:')
-po.level_print()
-
-""" make it so that you need tier 5 to fight the boss otherwise you cant """
-
-if po.level_print == 'Tier_5':
-    print('Checpoint_True')
-elif po.level_print != 'Tier_5':
-    print('Your not high level')
-
-
+class replay1():
+    def replay(x,y):
+        l = input('Would you like to fight ').upper()
+        if l == 'YES':
+            x
+            ask = input("Attack another enemy?").upper()
+            while ask == 'YES':
+                y.HP = 160
+                x
+                ask
+            if ask != 'YES':
+                print('wduowhudhwauhdwauhdwa')
+                
+    
+replay1.replay(test,B)
