14ea1fc74ef91c3c0ab366e7fdc0c819b0d698ce
Constantine-nagornuk <conplay55@gmail.com>
2023-05-25 10:52:56 -0400

    class attack fix

--- Orderly.py
+++ Orderly.py
@@ -5,52 +5,52 @@ from base_file import Base_class, B
 class sigil_Knight(Base_class):
     def __init__(self, HP, Weapon, XP):
         super().__init__(HP, Weapon, XP)
-    def Attack(self):
-        while B.HP > 0:
-            if B.HP < 0:
+    def Attack(self,x,y):
+        while x.HP > 0:
+            if x.HP < 0:
                 break  
-            OL = input('Damage the enemy?').upper()
+            OL = input('Swing your weapon at the enemy?').upper()
             if OL == 'YES':
-                Number_of_hits = random.randint(0 , 5)
-                Damage = int(25)
+                Number_of_hits = random.randint(0 , 4)
+                Damage = y
                 Total_DMG = Number_of_hits*Damage
-                Remaing_enemy_HP = B.HP - Total_DMG
+                Remaing_enemy_HP = x.HP - Total_DMG
                 if Remaing_enemy_HP < 0 or Remaing_enemy_HP == 0:
                     print('You defeated this enemy')
-                    Added_XP = B.XP + A.XP
+                    Added_XP = x.XP + A.XP
                     A.XP = Added_XP
                     print('You now have,' , Added_XP , 'XP')
                     break
-                B.HP = Remaing_enemy_HP
+                x.HP = Remaing_enemy_HP
                 print(Remaing_enemy_HP)
             if OL != 'YES':
                 print('You dont attack')
-                break
+                break    
                 
 class Whisperer(Base_class):
     def __init__(self, HP, Weapon, XP):
         super().__init__(HP, Weapon, XP)             
-    def Attack(self):
-        while B.HP > 0:
-            if B.HP < 0:
+    def Attack(self,x,y):
+        while x.HP > 0:
+            if x.HP < 0:
                 break  
-            OL = input('Damage the enemy?').upper()
+            OL = input('Swing your weapon at the enemy?').upper()
             if OL == 'YES':
-                Number_of_hits = random.randint(0 , 6)
-                Damage = int(15)
+                Number_of_hits = random.randint(0 , 4)
+                Damage = y
                 Total_DMG = Number_of_hits*Damage
-                Remaing_enemy_HP = B.HP - Total_DMG
+                Remaing_enemy_HP = x.HP - Total_DMG
                 if Remaing_enemy_HP < 0 or Remaing_enemy_HP == 0:
                     print('You defeated this enemy')
-                    Added_XP = B.XP + A.XP
-                    A.XP = Added_XP
+                    Added_XP = x.XP + C.XP
+                    C.XP = Added_XP
                     print('You now have,' , Added_XP , 'XP')
                     break
-                B.HP = Remaing_enemy_HP
+                x.HP = Remaing_enemy_HP
                 print(Remaing_enemy_HP)
             if OL != 'YES':
                 print('You dont attack')
-                break
+                break    
 
 A = sigil_Knight(100 , 'something' , 0)
 C = Whisperer(150 , 'something' , 0)
